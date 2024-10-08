e2f3e476a03adde7409713743bac7685d52f15ef
OriLaufer <orilaufer15@gmail.com>
2024-10-08 18:40:20 +0300

    first version

--- app/src/main/java/com/example/eggproject/MainActivity.java
+++ app/src/main/java/com/example/eggproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.eggproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+     private int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        counter =10;
+        TextView counterTextView = findViewById(R.id.TextView_counter);
+        counterTextView.setText(""+counter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void knock(View view) {
+        if (counter > 0) {
+            counter--;
+            TextView counterTextView = findViewById(R.id.TextView_counter);
+            counterTextView.setText("" + counter);
+
+        if (counter == 0) {
+            ImageView eggImageView = findViewById(R.id.ImageView_egg);
+            eggImageView.setImageResource(R.drawable.suprise);
+        }
+      }
+    }
+    public void reset(View view) {
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.TextView_counter);
+        counterTextView.setText("" + counter);
+        ImageView eggImageView = findViewById(R.id.ImageView_egg);
+        eggImageView.setImageResource(R.drawable.perple_egg);
     }
 }
